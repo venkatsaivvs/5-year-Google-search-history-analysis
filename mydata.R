6edffd9072c643dedad9053408bbea41539dad4d
venkatasai <venkatsaivvs@gmail.com>
2016-07-26 18:22:31 -0400

    Update mydata.R

--- mydata.R
+++ mydata.R
@@ -7,23 +7,53 @@ typeof(mydata$time)
 
 
 mydata$time<-as.POSIXct((mydata$timestamp_usec/1000000), origin="1970-01-01",tz="EST")
+#mydata$onlydate<-as.POSIXct(mydata$time,format='%m/%d/%Y',format='%m/%d/%Y')
+mydata$onlydate<-as.Date(mydata$time)
 
 mydata$day<-weekdays(mydata$time)
 mydata$month<-months(mydata$time)
 mydata$date<-as.Date(mydata$time)
-mydata$year<-as.numeric(format(x,'%Y'))
+mydata$year<-as.numeric(format(mydata$time,'%Y'))
 
+typeof(mydata$onlydate)
+table(mydata$onlydate)
 head(mydata$time)
+
 
 barplot(table(mydata$year),col=rainbow(4),main = "VENKAT ALL TIMESEARCHES BY YEAR")
 
+library(ggplot2)
 
+ggplot(mydata, aes(factor(mydata$year)))+ geom_bar()
+a<-qplot(factor(mydata$year), data=mydata, geom="bar", fill=factor(mydata$year),xlab = 'Year',ylab = 'Number of Searches',main = 'Searches by Year')
 
-
+a+labs(fill="Year")
 
 length(mydata$time)
 
 length(mydata$day)
+
+plot(table(mydata$onlydate))
+
+temp<-as.data.frame(table(mydata$onlydate))
+
+ggplot(temp,x=Day,y=count)
+qplot(Day, Count, data=temp, geom="line")
+
+str(temp)
+summary(temp)
+
+names(temp)<-c('Day','Count')
+str(temp)
+
+lineplot(temp$Day,temp$Count)
+
+timeseries<-ts(temp,start = 2013,end = 2016)
+
+plot(timeseries)
+
+
+ggplot(temp,aes(Var1,Freq)) + geom_line()
 
 tapply(mydata$time)
 
